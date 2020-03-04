2cd136086218668df0da07a85999a5fc18f94234
kimjaehu <kimjaehu@gmail.com>
2020-03-03 21:15:43 -0500

    added view logic for thumbnail

--- app/src/main/java/com/example/kotlinnews/MainAdapter.kt
+++ app/src/main/java/com/example/kotlinnews/MainAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinnews
 
+import android.text.TextUtils
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.article_row.view.*
 
 class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>() {
@@ -25,6 +28,19 @@ class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder
         val article = homeFeed.data.children.get(position)
         holder?.view?.textView_article_title?.text = article.data.title
         holder?.view?.textView_author?.text = "by ${article.data.author}"
+
+        val thumbnailImageView = holder.view.imageView_thumbnail
+
+        if(!TextUtils.isEmpty(article.data.thumbnail.trim()) && article.data.thumbnail.trim().isNotEmpty()){
+            if(Patterns.WEB_URL.matcher(article.data.thumbnail).matches()){
+                Picasso.get().load(article.data.thumbnail).into(thumbnailImageView)
+            }
+        } else {
+            holder.view.imageView_thumbnail.visibility = View.GONE
+        }
+
+
+
     }
 
 
